9544b00853a931cb707b18c2f8a732dda9493463
Neczpal Abel <neczpal@gmail.com>
2025-11-26 22:34:55 +0100

    don't show empty footer

--- src/layouts/LayoutDefault.tsx
+++ src/layouts/LayoutDefault.tsx
@@ -10,9 +10,11 @@ export function LayoutDefault({ children, footer, header }: { children: React.Re
             <main className="w-full flex-grow overflow-y-auto pb-32 px-12 max-w-[1024px]">
                 {children}
             </main>
-            <footer className="w-full border-t border-gray-200 dark:border-gray-700  flex-shrink-0 fixed bottom-0 left-0 right-0 bg-white dark:bg-gray-800 p-4">
+            {footer && (<footer
+                className="w-full border-t border-gray-200 dark:border-gray-700  flex-shrink-0 fixed bottom-0 left-0 right-0 bg-white dark:bg-gray-800 p-4">
                 {footer}
-            </footer>
+            </footer>)
+            }
         </>
     );
 }
