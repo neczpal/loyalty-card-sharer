afc6c8022d08bdec02e1b0c32d783fe73548c870
Neczpal Abel <neczpal@gmail.com>
2025-11-26 22:37:59 +0100

    fix warning

--- src/components/BaseIconButton.tsx
+++ src/components/BaseIconButton.tsx
@@ -11,16 +11,13 @@ export function BaseIconButton({onClick, text, iconName, variant = 'default'}: I
 
     const baseClasses = "flex g-1 items-center px-4 py-2 rounded-md";
 
-    let variantClasses = "";
+    let variantClasses = "bg-gray-200 text-gray-700 dark:bg-gray-700 dark:text-gray-200 hover:bg-gray-300 dark:hover:bg-gray-600";
     switch (variant) {
         case 'primary':
             variantClasses = "bg-blue-500 text-white hover:bg-blue-600 dark:bg-blue-600 dark:hover:bg-blue-700";
             break;
         case 'danger':
             variantClasses = "bg-red-500 text-white hover:bg-red-600 dark:bg-red-600 dark:hover:bg-red-700";
-            break;
-        default: // 'default'
-            variantClasses = "bg-gray-200 text-gray-700 dark:bg-gray-700 dark:text-gray-200 hover:bg-gray-300 dark:hover:bg-gray-600";
             break;
     }
 
